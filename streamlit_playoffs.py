5e0bf0e4ff772da2aac2b30dae62d70f6aa59bcc
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2023-01-12 10:25:42 -0500

    Update streamlit_playoffs.py

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -33,5 +33,5 @@ st.header('Title Chances!')
 #      'Team': ["01", "02", "03", "04", "05", "06", "07"],
 #      'Win Probability': [10, 20, 10, 15, 25, 5, 15]
 #      })
-# fig = px.pie(sheets_df, values='prob', names='Team', title='Title Chances')
-# st.plotly_chart(fig, use_container_width=True)
+fig = px.pie(sheets_df, values='prob', names='Team', title='Title Chances')
+st.plotly_chart(fig, use_container_width=True)
