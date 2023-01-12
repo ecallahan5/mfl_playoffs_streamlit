7b340e5e5adef0b563f15236193fd41b1dcd3c72
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2023-01-12 10:22:52 -0500

    Update streamlit_playoffs.py

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -23,6 +23,7 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 sheets_df = run_query(f'SELECT * FROM "{sheet_url}"')
 
+st.write(sheets_df)
 
 st.header('2023 On The Door Forecasts')
 st.markdown("""---""")
@@ -32,5 +33,5 @@ st.header('Title Chances!')
 #      'Team': ["01", "02", "03", "04", "05", "06", "07"],
 #      'Win Probability': [10, 20, 10, 15, 25, 5, 15]
 #      })
-fig = px.pie(sheets_df, values='prob', names='Team', title='Title Chances')
-st.plotly_chart(fig, use_container_width=True)
+# fig = px.pie(sheets_df, values='prob', names='Team', title='Title Chances')
+# st.plotly_chart(fig, use_container_width=True)
