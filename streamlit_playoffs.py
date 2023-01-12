4603b289ad42e93224bd4696a7a43a01fcd44ed1
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2023-01-12 10:26:44 -0500

    Update streamlit_playoffs.py

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -29,9 +29,5 @@ st.header('2023 On The Door Forecasts')
 st.markdown("""---""")
 
 st.header('Title Chances!')
-# champ = pd.DataFrame({
-#      'Team': ["01", "02", "03", "04", "05", "06", "07"],
-#      'Win Probability': [10, 20, 10, 15, 25, 5, 15]
-#      })
 fig = px.pie(sheets_df, values='prob', names='Team', title='Title Chances')
 st.plotly_chart(fig, use_container_width=True)
