0b71546b949131a20817d6ae145dd6434f2b220a
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2023-01-23 15:44:45 -0500

    Update streamlit_playoffs.py

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -8,7 +8,7 @@ from gsheetsdb import connect
 
 round_select = st.radio(
     "Choose the Round to View",
-    ('Wild Card', 'Divisional'))
+    ('Wild Card', 'Divisional', 'Conference))
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -42,7 +42,16 @@ wc_df = pd.DataFrame(run_query(f'SELECT * FROM "{wc_sheet_url}"'))
 fig1 = px.pie(wc_df, values='title_chance', names='Team', title='Before Divisional Week')
 # st.plotly_chart(fig1, use_container_width=True)
 
+# Update with Division Round Results
+wc_sheet_url = st.secrets["gsheets"]["div_data_url"]
+div_df = pd.DataFrame(run_query(f'SELECT * FROM "{div_sheet_url}"'))
+
+fig2 = px.pie(div_df, values='title_chance', names='Team', title='Before Conference Champ Week')
+# st.plotly_chart(fig1, use_container_width=True)
+
 if round_select == 'Wild Card':
     st.plotly_chart(fig, use_container_width=True)
+if round_select == 'Divisional':
+        st.plotly_chart(fig1, use_container_width=True)
 else:
-    st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
