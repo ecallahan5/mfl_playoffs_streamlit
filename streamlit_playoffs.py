ca472600e1d22cd25813a46e393605403804bedd
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2024-01-10 20:59:34 -0500

    New 2024 Draft

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -35,31 +35,31 @@ fig = px.pie(sheets_df, values='prob', names='Team', title='Before Wild Card Wee
 # st.plotly_chart(fig, use_container_width=True)
 
 
-# Update with Wild Card Results
-wc_sheet_url = st.secrets["gsheets"]["wc_data_url"]
-wc_df = pd.DataFrame(run_query(f'SELECT * FROM "{wc_sheet_url}"'))
+# # Update with Wild Card Results
+# wc_sheet_url = st.secrets["gsheets"]["wc_data_url"]
+# wc_df = pd.DataFrame(run_query(f'SELECT * FROM "{wc_sheet_url}"'))
 
-fig1 = px.pie(wc_df, values='title_chance', names='Team', title='Before Divisional Week')
-# st.plotly_chart(fig1, use_container_width=True)
+# fig1 = px.pie(wc_df, values='title_chance', names='Team', title='Before Divisional Week')
+# # st.plotly_chart(fig1, use_container_width=True)
 
-# Update with Division Round Results
-div_sheet_url = st.secrets["gsheets"]["div_data_url"]
-div_df = pd.DataFrame(run_query(f'SELECT * FROM "{div_sheet_url}"'))
+# # Update with Division Round Results
+# div_sheet_url = st.secrets["gsheets"]["div_data_url"]
+# div_df = pd.DataFrame(run_query(f'SELECT * FROM "{div_sheet_url}"'))
 
-fig2 = px.pie(div_df, values='title_chance', names='Team', title='Before Conference Champ Week')
+# fig2 = px.pie(div_df, values='title_chance', names='Team', title='Before Conference Champ Week')
 
-# Update with Conference Final Results
+# # Update with Conference Final Results
 
-conf_sheet_url = st.secrets["gsheets"]["conf_data_url"]
-conf_df = pd.DataFrame(run_query(f'SELECT * FROM "{conf_sheet_url}"'))
+# conf_sheet_url = st.secrets["gsheets"]["conf_data_url"]
+# conf_df = pd.DataFrame(run_query(f'SELECT * FROM "{conf_sheet_url}"'))
 
-fig3 = px.pie(conf_df, values='title_chance', names='Team', title='Before Super Bowl')
+# fig3 = px.pie(conf_df, values='title_chance', names='Team', title='Before Super Bowl')
 
-if round_select == 'Wild Card':
-    st.plotly_chart(fig, use_container_width=True)
-if round_select == 'Divisional':
-        st.plotly_chart(fig1, use_container_width=True)
-if round_select == 'Conference':
-        st.plotly_chart(fig2, use_container_width=True)
-else:
-    st.plotly_chart(fig3, use_container_width=True)
+# if round_select == 'Wild Card':
+#     st.plotly_chart(fig, use_container_width=True)
+# if round_select == 'Divisional':
+#         st.plotly_chart(fig1, use_container_width=True)
+# if round_select == 'Conference':
+#         st.plotly_chart(fig2, use_container_width=True)
+# else:
+#     st.plotly_chart(fig3, use_container_width=True)
