b69419a1fa7c5f0494db53c91c584eb7dcaec68d
ecallahan5 <52139577+ecallahan5@users.noreply.github.com>
2024-01-16 09:41:56 -0500

    Update streamlit_playoffs.py

--- streamlit_playoffs.py
+++ streamlit_playoffs.py
@@ -44,7 +44,7 @@ wc_df = pd.DataFrame(run_query(f'SELECT * FROM "{wc_sheet_url}"'))
 
 # fig1 = px.pie(wc_df, values='title_chance', names='Team', title='Before Divisional Week')
 # st.plotly_chart(fig1, use_container_width=True)
-# st.bar_chart(wc_df, x='Team', y='title_chance', title='Before Divisional Week')
+st.bar_chart(wc_df, x='Team', y='title_chance', title='Before Divisional Week')
 
 
 # # Update with Division Round Results
